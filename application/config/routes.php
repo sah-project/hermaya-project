ee536f0b5ec829be5205dd28d3e86755e5a8ea46
sugi <sugiantosattok@gmail.com>
2019-10-01 21:48:00 +0800

    Fix Template

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
-$route['landing'] = 'welcome/landing';
+$route['default_controller'] = 'syslanding';
+$route['test'] = 'syslanding/test';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
---
+++ application/controllers/syslanding.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class syslanding extends CI_Controller {
+
+   public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
+	public function index()
+	{
+		$this->load->view('dashboard/index');
+	}
+
+	
+}
