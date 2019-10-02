9292de080513e4e7ce5be33351d0c1d6e36e3c90
sugi <sugiantosattok@gmail.com>
2019-10-02 15:32:46 +0800

    start part admin/manageberita

--- application/views/admin/pages/index.php
+++ application/views/admin/pages/index.php
@@ -41,91 +41,33 @@
 								<form class="validate" action="php/contact.php" method="post" enctype="multipart/form-data" data-success="Sent! Thank you!" data-toastr-position="top-right">
 									<fieldset>
 										<!-- required [php action request] -->
-										<input type="hidden" name="action" value="contact_send" />
-
 										<div class="row">
 											<div class="form-group">
 												<div class="col-md-6 col-sm-6">
-													<label>First Name *</label>
-													<input type="text" name="contact[first_name]" value="" class="form-control required">
-												</div>
-												<div class="col-md-6 col-sm-6">
-													<label>Last Name *</label>
-													<input type="text" name="contact[last_name]" value="" class="form-control required">
-												</div>
-											</div>
-										</div>
-
-										<div class="row">
-											<div class="form-group">
-												<div class="col-md-6 col-sm-6">
-													<label>Email *</label>
-													<input type="email" name="contact[email]" value="" class="form-control required">
-												</div>
-												<div class="col-md-6 col-sm-6">
-													<label>Phone *</label>
-													<input type="text" name="contact[phone]" value="" class="form-control required">
-												</div>
-											</div>
-										</div>
-
-										<div class="row">
-											<div class="form-group">
-												<div class="col-md-12 col-sm-12">
-													<label>Position *</label>
-													<select name="contact[position]" class="form-control pointer required">
-														<option value="">--- Select ---</option>
-														<option value="Marketing">PR &amp; Marketing</option>
-														<option value="Developer">Web Developer</option>
-														<option value="php">PHP Programmer</option>
-														<option value="Javascript">Javascript Programmer</option>
-													</select>
-												</div>
-											</div>
-										</div>
-
-										<div class="row">
-											<div class="form-group">
-												<div class="col-md-6 col-sm-6">
-													<label>Expected Salary *</label>
+													<label>Judul Berita *</label>
 													<input type="text" name="contact[expected_salary]" value="" class="form-control required">
 												</div>
 												<div class="col-md-6 col-sm-6">
-													<label>Start Date *</label>
+													<label>Tanggal Posting *</label>
 													<input type="text" name="contact[start_date]" value="" class="form-control datepicker required" data-format="yyyy-mm-dd" data-lang="en" data-RTL="false">
 												</div>
 											</div>
 										</div>
-
 										<div class="row">
 											<div class="form-group">
 												<div class="col-md-12 col-sm-12">
-													<label>Experience *</label>
-													<textarea name="contact[experience]" rows="4" class="form-control required"></textarea>
+													<label>Text Berita *</label>
+													<textarea class="form-control summernote" name="pertanyaan" id="pertanyaan"></textarea>
 												</div>
 											</div>
 										</div>
-
-										<div class="row">
-											<div class="form-group">
-												<div class="col-md-12 col-sm-12">
-													<label>
-														Website
-														<small class="text-muted">- optional</small>
-													</label>
-													<input type="text" name="contact[website]" placeholder="http://" class="form-control">
-												</div>
-											</div>
-										</div>
-
 										<div class="row">
 											<div class="form-group">
 												<div class="col-md-12">
 													<label>
 														File Attachment 
-														<small class="text-muted">Curriculum Vitae - optional</small>
+														<small class="text-muted">Foto Berita</small>
 													</label>
-
 													<!-- custom file upload -->
 													<div class="fancy-file-upload fancy-file-primary">
 														<i class="fa fa-upload"></i>
@@ -133,7 +75,7 @@
 														<input type="text" class="form-control" placeholder="no file selected" readonly="" />
 														<span class="button">Choose File</span>
 													</div>
-													<small class="text-muted block">Max file size: 10Mb (zip/pdf/jpg/png)</small>
+													<small class="text-muted block">Max file size: 10Mb (jpeg/jpg/png)</small>
 
 												</div>
 											</div>
@@ -144,8 +86,7 @@
 									<div class="row">
 										<div class="col-md-12">
 											<button type="submit" class="btn btn-3d btn-teal btn-xlg btn-block margin-top-30">
-												SEND APPLICATION
-												<span class="block font-lato">We'll get back to you within 48 hours</span>
+												POSTING BERITA
 											</button>
 										</div>
 									</div>
