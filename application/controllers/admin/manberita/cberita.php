7d1794a84550d60673d3b3b0d72e3a58f9140bc3
sugi <sugiantosattok@gmail.com>
2019-10-03 13:57:50 +0800

    crud berita/insert n upload berita done

--- application/controllers/admin/manberita/cberita.php
+++ application/controllers/admin/manberita/cberita.php
@@ -23,17 +23,17 @@ class cberita  extends CI_Controller {
     }
 
     public function simpanberita (){          
-
-
         if (!empty($_POST)) {   
             $data = array(
                 'tanggal' => $this->security->xss_clean($this->input->post('tanggal')),
                 'judul' =>$this->security->xss_clean($this->input->post('judul')),
                 'konten' =>$this->input->post('konten'),
+                'image' =>$_FILES['image']['name'],
                 );
             $action = $this->model_berita->simpan_berita($data);
                 if ($action==true) {
-                    redirect('admin');
+                    $this->image = $this->_uploadImage();
+                    redirect('admin/manage-berita');
                 } else {
                     echo "something wrong";
                 }  
@@ -41,6 +41,27 @@ class cberita  extends CI_Controller {
           redirect('master');
         } 
     }
+
+    private function _uploadImage()
+    {
+        $config['upload_path']          = './upload/berita/';
+        $config['allowed_types']        = 'gif|jpg|png';
+        $config['file_name']            = $_FILES['image']['name'];
+        $config['overwrite']            = true;
+        $config['max_size']             = 1024; // 1MB
+        if( ! $this->upload->initialize($config)){
+                    $error = array('error' => $this->upload->display_errors());
+                    echo "Error";
+                }
+
+        $this->load->library('upload', $config);
+        if ($this->upload->do_upload('image')) {
+            return $this->upload->data("file_name");
+        }
+        print_r($this->upload->display_errors());
+       //return "default.jpg";
+    }
+
 
        public function show_berita(){
         $data = array (
